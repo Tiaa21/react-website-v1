dca12656ea848339c472794f5153f1aa936687b8
Tiaaa <132942390+Tiaa21@users.noreply.github.com>
2023-05-09 07:47:52 +0000

    Update 21 files

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
+import AboutUs from './components/pages/AboutUs';
+import Maps from './components/pages/Maps';
 import SignUp from './components/pages/SignUp';
 
 function App() {
@@ -14,8 +14,8 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
+          <Route path='/maps' component={Maps} />
+          <Route path='/aboutus' component={AboutUs} />
           <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import '../../App.css';
-import Cards from '../Cards';
-import HeroSection from '../HeroSection';
-import Footer from '../Footer';
+import React from "react";
+import "../../App.css";
+import HeroSection from "../HeroSection";
 
 function Home() {
   return (
     <>
       <HeroSection />
-      <Cards />
-      <Footer />
     </>
   );
 }
